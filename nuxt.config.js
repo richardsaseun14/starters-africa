7c47d9042ab6a6d672a0d657e7373df9e7c9ecc6
Richard Saseun <richardsaseun@gmail.com>
2020-05-24 22:21:53 +0100

    bug(favicon fix): add favicons for all platforms

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,15 +8,22 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { name: 'google-site-verification', content: 'IgQbV8Myd44n69-_WPMxZtU-eKXEN64Qt9PRaXiyPr0' }
+      { name: 'google-site-verification', content: 'IgQbV8Myd44n69-_WPMxZtU-eKXEN64Qt9PRaXiyPr0' },
+      { name: 'theme-color', content: '#f8ad00' },
+      { name: 'msapplication-TileColor', content: '#f8ad00' },
+      { name: 'msapplication-TileImage', content: '/mstile-144x144.png?v=oLJrmap688' }
       // {
       //   hid: 'description',
       //   name: 'description',
       //   content: process.env.npm_package_description || ''
       // }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }],
-    link: [{ rel: 'shortcut icon', href: '/favicon.png' }]
+    link: [{ rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png?v=oLJrmap688' }],
+    link: [{ rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png?v=oLJrmap688' }],
+    link: [{ rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png?v=oLJrmap688' }],
+    link: [{ rel: 'mask-icon', color: '#5bbad5', href: '/safari-pinned-tab.svg?v=oLJrmap688' }],
+    link: [{ rel: 'shortcut icon', href: '/favicon.ico?v=oLJrmap688' }],
+    link: [{ rel: 'manifest', href: '/site.webmanifest?v=oLJrmap688' }]
   },
   /*
    ** Customize the progress-bar color
